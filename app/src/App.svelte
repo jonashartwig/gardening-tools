0772229b362638420d2805e0e09e23b7ffbe749d
cvv556 <jonas.hartwig@teliacompany.com>
2021-03-24 17:08:50 +0100

    stated work on bootstrap

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -16,17 +16,31 @@
 </script>
 
 <main>
-	{#if state}
-	<Courses state={state} />
-	The current sun altitude: {round(state.currentAltitude)}°. <br />
-	The highest altitude today is: {round(state.today.noon.altitude)}°. <br />
-	The sun rises at about {state.today.sunrise.time.toLocaleTimeString()}, reaches it highest point at about {state.today.noon.time.toLocaleTimeString()} and sets at about {state.today.sunset.time.toLocaleTimeString()}. <br />
-	<br />
-	The shadow is {round(state.today.noon.shadowMultiplier)} times as long as the actual object at the highest altitude. <br />
-	The shadow is {round(state.shadowMultiplierAtCurrentAltitude)} times as long as the actual object at the current altitude.
-	{:else}
-		Please wait while we determine your position.
-		<br />
-		<div class="lds-ripple"><div></div><div></div></div>
-	{/if}
+	<div class="container">
+		{#if state}
+			<Courses state={state} />
+			<div class="row">
+				<div class="col">
+					The current sun altitude: {round(state.currentAltitude)}°. <br />
+				</div>
+				<div class="w-100"></div>
+				<div class="col">
+					The shadow is {round(state.shadowMultiplierAtCurrentAltitude)} times as long as the actual object at the current altitude.
+				</div>
+				<div class="w-100" />
+				<div class="col">
+					The shadow is {round(state.today.noon.shadowMultiplier)} times as long as the actual object at the highest altitude.
+				</div>
+			</div>
+		{:else}
+			<div class="row">
+				<div class="col">
+					<p class="content-justify-center">
+						Please wait while we determine your position.
+					</p>
+					<div class="lds-ripple"><div></div><div></div></div>
+				</div>
+			</div>
+		{/if}
+	</div>
 </main>
--- app/src/components/Courses.svelte
+++ app/src/components/Courses.svelte
@@ -1,7 +1,6 @@
 <script>
-import SunCourse from "../course";
-
     import TreeAnimation from "./TreeAnimation.svelte";
+	import round from "../service/round";
     
     export let state = undefined;
 
@@ -15,13 +14,25 @@ import SunCourse from "../course";
     }
 </script>
 
-<ol class="nav justify-content-end">
-    {#each state.sortedDates as date}
-        <li class="nav-item" on:click|preventDefault|stopPropagation={() => select(date)}>
-            { date.translationKey }
-        </li>
-    {/each}
-</ol>
+<div class="row">
+    <ol class="nav justify-content-center">
+        {#each state.sortedDates as date}
+            <li class="nav-item" on:click|preventDefault|stopPropagation={() => select(date)}>
+                <a class="nav-link active" href="#">{ date.translationKey }</a>
+            </li>
+        {/each}
+    </ol>
+</div>
+
+<div class="row">
+    <div class="col">
+        The highest altitude is: {round(selected.noon.altitude)}°.
+    </div>
+    <div class="w-100" />
+    <div class="col">
+        The sun rises at about {selected.sunrise.time.toLocaleTimeString()}, reaches it highest point at about {selected.noon.time.toLocaleTimeString()} and sets at about {selected.sunset.time.toLocaleTimeString()}.
+    </div>
+</div>
 
 {#if mounted}
     <TreeAnimation state={selected} />
